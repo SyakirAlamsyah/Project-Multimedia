5719163c48521d86f2fcc68ea94b276c2de398ed
Chiwassu <kristoforusnoventa@gmail.com>
2026-06-22 11:04:27 +0700

    tes

--- Assets/Script/main.cs
+++ Assets/Script/main.cs
@@ -7,7 +7,7 @@ public class main : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        ppp;
+        bobi;
     }
 
     // Update is called once per frame
