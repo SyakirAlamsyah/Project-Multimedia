147eb89b9532d39dbea1746ae4610522019e5de4
Chiwassu <kristoforusnoventa@gmail.com>
2026-06-22 22:03:52 +0700

    reset

--- Assets/Script/main.cs
+++ Assets/Script/main.cs
@@ -4,12 +4,11 @@ using UnityEngine;
 
 public class main : MonoBehaviour
 {
-    public GameObject bobi;
     // Start is called before the first frame update
     //ini adlh komen 
     void Start()
     {
-        bobi;
+
     }
 
     // Update is called once per frame
