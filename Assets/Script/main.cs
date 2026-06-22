0da40d3f7ae5223aae2e03087a6889ab0f85bd4f
DWIHEXA <oktabimo17dwi@gmail.com>
2026-06-22 11:25:27 +0700

    coba merge conflict

--- Assets/Script/main.cs
+++ Assets/Script/main.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class main : MonoBehaviour
 {
-    public GameObject bobi;
     // Start is called before the first frame update
+    //ini adlh komen 
     void Start()
     {
-        bobi;
+       halo;
     }
 
     // Update is called once per frame
