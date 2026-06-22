315582f0cf9dc63596957f5fe40d601cd4c34b9c
Chiwassu <kristoforusnoventa@gmail.com>
2026-06-22 11:24:20 +0700

    bersihin noda

--- Assets/Script/main.cs
+++ Assets/Script/main.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public class main : MonoBehaviour
 {
-    public GameObject bobi;
     // Start is called before the first frame update
     void Start()
     {
-        bobi;
+        
     }
 
     // Update is called once per frame
